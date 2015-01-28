d40f3e95194fdfb5c2b434d10de1e659c7abbcc5
oke <s1420616@u.tsukuba.ac.jp>
2015-01-28 15:50:13 +0900

    OKE: Ruby

---
+++ oke.rb
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Rangeのeachとcase文を使ったシンプルなやつ
+(1..100).each do |i|
+  case
+  when i % 15 == 0
+    puts "FizzBuzz"
+  when i % 5 == 0
+    puts  "Buzz"
+  when i % 3 == 0
+    puts "Fizz"
+  else
+    puts i
+  end
+end
+
+puts "------------------------------------"
+
+# 三項演算子をネストしたワンライナー
+ (1..100).each {|i| puts ((i % 15).zero? ? "FizzBuzz" : ((i % 5).zero? ? "Buzz" : ((i % 3).zero? ? "Fizz" : i))) }
