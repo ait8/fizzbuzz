a5b2e06c397053d7f4f4e3c1e1a317dcc8822d12
Shota Furuya <s1420693@u.tsukuba.ac.jp>
2015-01-28 16:07:23 +0900

    FURUYA: Java

---
+++ Furuya.java
@@ -0,0 +1,37 @@
+public class Furuya {
+	public static void main(String[] args) {
+		FizzBuzz fizzbuzz = new FizzBuzz(100);
+		fizzbuzz.doFizzBuzz();
+	}
+}
+
+class FizzBuzz {
+	private int n;
+	
+	public FizzBuzz(int n) {
+		this.n = n;
+	}
+
+	public void doFizzBuzz() {
+		for (int i = 1; i <= 100; i++) {
+			String s = getFizzOrBuzz(i);
+			System.out.println(s);
+		}
+	}
+
+	private String getFizzOrBuzz(int x) {
+		StringBuilder sb = new StringBuilder();
+		if (x % 3 == 0) {
+			sb.append("fizz");
+		}
+		if (x % 5 == 0) {
+			sb.append("buzz");
+		}
+		String s = sb.toString();
+
+		if (s.equals("")) {
+			s = Integer.valueOf(x).toString();
+		}
+		return s;
+	}
+}
