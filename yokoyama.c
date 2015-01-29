8052e4087c26b02872c6c11abd1c91ccd0206852
Kengo Yokoyama <rbken.lo.ovl@gmail.com>
2015-01-29 17:09:07 +0900

    Add shortest C-FizzBuzz code

--- yokoyama.c
+++ yokoyama.c
@@ -1,3 +1,6 @@
 // cc yokoyama.c
 // ./a.out
-main(i){for(;i<101;)printf(i%3?i%5?"%d\n":"Buzz\n":i%5?"Fizz\n":"FizzBuzz\n",i++);}
+// main(i){for(;i<101;)printf(i%3?i%5?"%d\n":"Buzz\n":i%5?"Fizz\n":"FizzBuzz\n",i++);}
+
+// compiler-dependent code
+main(i){for(;i<101;puts("Buzz"-i*i++%5))printf(i%3?i%5?"%d":0:"Fizz",i);}
